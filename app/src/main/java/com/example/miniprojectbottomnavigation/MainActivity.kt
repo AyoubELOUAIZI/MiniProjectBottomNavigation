20f050a499a534b7f3d9567392decc0f381da8f0
ELOUAIZI Ayoub <ayoubelouaizi@gmail.com>
2023-12-03 22:32:36 +0100

    small changes

--- app/src/main/java/com/example/miniprojectbottomnavigation/MainActivity.kt
+++ app/src/main/java/com/example/miniprojectbottomnavigation/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.miniprojectbottomnavigation
 
+import android.animation.ObjectAnimator
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.animation.OvershootInterpolator
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import androidx.fragment.app.Fragment
 import com.example.miniprojectbottomnavigation.fragment.ContactFragment
@@ -10,6 +13,7 @@ import com.example.miniprojectbottomnavigation.fragment.HomeFragment
 import com.example.miniprojectbottomnavigation.fragment.SettingFragment
 import com.qamar.curvedbottomnaviagtion.CurvedBottomNavigation
 import androidx.activity.viewModels
+import androidx.core.animation.doOnEnd
 
 class MainActivity : AppCompatActivity() {
     private val viewModel by viewModels<MainViewModel>()
@@ -18,6 +22,32 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen().apply {
             setKeepOnScreenCondition {
                 !viewModel.isReady.value
+            }
+            setOnExitAnimationListener {screen->
+                val zoomX=ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_X,
+                    0.4f,
+                    0.0f
+                )
+                zoomX.interpolator=OvershootInterpolator()
+                zoomX.duration=300L
+                zoomX.doOnEnd { screen.remove() }
+
+                val zoomY=ObjectAnimator.ofFloat(
+                    screen.iconView,
+                    View.SCALE_Y,
+                    0.4f,
+                    0.0f
+                )
+                zoomY.interpolator=OvershootInterpolator()
+                zoomY.duration=300L
+                zoomY.doOnEnd { screen.remove() }
+
+                zoomX.start()
+                zoomY.start()
+
+
             }
         }
         setContentView(R.layout.activity_main)
